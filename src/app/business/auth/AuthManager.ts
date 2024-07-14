cab565b386bb6364ea3699865324a202e93b6ad2
Herquiloide Hele <herquiloide.hele@wit-software.com>
2024-07-14 23:59:25 +0100

    video controls

--- src/app/business/auth/AuthManager.ts
+++ src/app/business/auth/AuthManager.ts
@@ -5,7 +5,6 @@ import { FirebaseCollections } from "@/utils/Constants";
 import { IUser } from "@/app/business/auth/UsersData";
 
 class AuthManager {
-  private authUser: IUser | undefined;
   private readonly LOG_TAG = "AuthManager";
 
   public async getAuthUser(): Promise<IUser | undefined> {
@@ -20,32 +19,14 @@ class AuthManager {
         return;
       }
 
-      if (this.authUser) {
-        Logger.info(this.LOG_TAG, "Get user from local data", [this.authUser]);
-        return this.authUser;
-      }
-
       Logger.info(this.LOG_TAG, "User found", [user]);
 
-      const authUser = await FirestoreService.getDocumentById(FirebaseCollections.USERS, user?.email);
-      this.authUser = authUser as IUser;
+      const authUser = (await FirestoreService.getDocumentById(FirebaseCollections.USERS, user?.email)) as IUser;
 
       Logger.info(this.LOG_TAG, "Auth user found", [authUser]);
       return authUser as IUser;
     } catch (error) {
       Logger.error(this.LOG_TAG, "Error getting auth user", [error]);
-    }
-  }
-
-  public async signOut(): Promise<void> {
-    Logger.debug(this.LOG_TAG, "Signing out");
-
-    try {
-      this.authUser = undefined;
-
-      Logger.debug(this.LOG_TAG, "Signed out successfully");
-    } catch (error) {
-      Logger.error(this.LOG_TAG, "Error signing out", [error]);
     }
   }
 }
--- src/components/course/video-player/VideoPlayer.tsx
+++ src/components/course/video-player/VideoPlayer.tsx
@@ -6,7 +6,7 @@ export default function VideoPlayer(props: VideoPlayerProps) {
   return (
     <div className={"h-full w-full"}>
       <iframe
-        src={`https://www.youtube.com/embed/${props.videoId}?si=jNvGwj--YLRDaLNz&amp;controls=0&amp;showinfo=0&amp;rel=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp`}
+        src={`https://www.youtube.com/embed/${props.videoId}?si=jNvGwj--YLRDaLNz&amp;controls=1&amp;showinfo=0&amp;rel=0&amp;autoplay=1&amp;mute=1&amp;loop=1&amp`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"

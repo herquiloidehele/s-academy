745440fdd4f7b4697da1f7608ebdd122421fa481
Herquiloide Hele <herquiloide.hele@wit-software.com>
2024-07-14 17:59:55 +0100

    env variables

--- src/app/services/FirebaseConfig.ts
+++ src/app/services/FirebaseConfig.ts
@@ -3,12 +3,25 @@ import Logger from "@/utils/Logger";
 
 import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { base64ToObject } from "@/lib/utils";
 import Firestore = firestore.Firestore;
 
 class FirebaseConfig {
   private firebaseApp: ReturnType<typeof initializeApp> | null = null;
   private readonly LOG_TAG = "FirebaseConfig";
+
+  private firebaseConfig: any = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: "googleapis.com",
+  };
 
   public constructor() {
     this.initialize();
@@ -19,7 +32,7 @@ class FirebaseConfig {
   }
 
   public initialize(): void {
-    Logger.debug(this.LOG_TAG, "Initializing Firebase Admin", [process.env.FIREBASE_SERVICE_ACCOUNT_KEY]);
+    Logger.debug(this.LOG_TAG, "Initializing Firebase Admin", [this.firebaseConfig]);
 
     try {
       if (this.firebaseApp) {
@@ -27,12 +40,8 @@ class FirebaseConfig {
         return;
       }
 
-      const serviceAccountObject = base64ToObject(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
-
-      Logger.debug(this.LOG_TAG, "Service Account Object", [serviceAccountObject]);
-
       this.firebaseApp = initializeApp({
-        credential: cert(serviceAccountObject),
+        credential: cert(this.firebaseConfig),
       });
 
       Logger.debug(this.LOG_TAG, "Firebase Admin initialized");

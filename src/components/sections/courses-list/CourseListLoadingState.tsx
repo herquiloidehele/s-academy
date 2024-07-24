536745a262705458b1020bea23027315e42da3c7
Herquiloide Hele <herquiloide.hele@wit-software.com>
2024-07-24 22:32:36 +0100

    implement loading state

--- src/components/sections/courses-list/CourseListLoadingState.tsx
+++ src/components/sections/courses-list/CourseListLoadingState.tsx
@@ -1,3 +1,32 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function CourseListLoadingState() {
-  return <div>Loading...</div>;
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill_,_minmax(350px_,_1fr))] gap-x-4 gap-y-11">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <CourseEntryLoadingState key={index} />
+      ))}
+    </div>
+  );
+}
+
+function CourseEntryLoadingState() {
+  return (
+    <div className="flex flex-col gap-1">
+      <a className="block relative h-80 rounded overflow-hidden aspect-square">
+        <Skeleton className="object-cover object-center w-full h-full block bg-gray-300" />
+      </a>
+      <div className="mt-4">
+        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1 uppercase">
+          <Skeleton className="w-20 h-4 bg-gray-300" />
+        </h3>
+        <h2 className="text-gray-900 title-font text-lg font-medium">
+          <Skeleton className="w-40 h-6 bg-gray-300" />
+        </h2>
+        <p className="mt-1 text-green-400 font-bold">
+          <Skeleton className="w-12 h-4 bg-gray-300" />
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/sections/courses-list/CoursesList.tsx
+++ src/components/sections/courses-list/CoursesList.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import ChartIcon from "@/assets/icons/chart-icon.svg";
 import { motion } from "framer-motion";
-import { Constants } from "@/utils/Constants";
 import CourseEntry from "@/components/course/course-entry/CourseEntry";
 import { ICourse } from "@/app/backend/business/course/CourseData";
 
@@ -11,41 +9,17 @@ interface ICCourseListProps {
 }
 export default function CoursesList(props: ICCourseListProps) {
   return (
-    <div
-      className={"w-full py-14 px-4 lg:px-6 lg:py-24 bg-[rgba(217,255,229,15%)] relative scroll-mt-20"}
-      id={Constants.UI.SECTIONS.COURSES}
-    >
-      <div className={"max-w-[1300px] mx-auto "}>
-        <motion.div animate={{ y: [0, 10, 0] }} transition={{ duration: 1.5, repeat: Infinity }}>
-          <motion.span initial={{ x: -1000 }} whileInView={{ x: 0 }}>
-            <ChartIcon className="w-6 h-6 text-green-400" />
-          </motion.span>
+    <div className="grid grid-cols-[repeat(auto-fill_,_minmax(350px_,_1fr))] gap-x-4 gap-y-11">
+      {props.courses.map((course, index) => (
+        <motion.div
+          key={`${course.id}-${index}`}
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: index * 0.1 }}
+        >
+          <CourseEntry course={course} />
         </motion.div>
-
-        <div>
-          <h2 className={"text-2xl md:text-4xl font-bold flex justify-center gap-2 items-center"}>
-            <span className={"text-black"}>Principais</span>
-            <span className={"text-green-400"}> Cursos</span>
-          </h2>
-
-          <section className="text-gray-600 body-font">
-            <div className="container px-0 md:px-5 py-8 md:py-16 mx-auto">
-              <div className="grid grid-cols-[repeat(auto-fill_,_minmax(350px_,_1fr))] gap-x-4 gap-y-11">
-                {props.courses.map((course, index) => (
-                  <motion.div
-                    key={`${course.id}-${index}`}
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1, delay: index * 0.1 }}
-                  >
-                    <CourseEntry course={course} />
-                  </motion.div>
-                ))}
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }

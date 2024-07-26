f3e631117d82b4335182720a44655ac4e52be684
Ercilio Marques <erciliomarquesmanhica@gmail.com>
2024-07-26 21:12:10 +0200

    enhanced module actions buttons positioning

--- src/app/teacher/products/courses/components/ModuleItemList.tsx
+++ src/app/teacher/products/courses/components/ModuleItemList.tsx
@@ -8,7 +8,7 @@ function ModuleItemList({ module, index }) {
   return (
     <AccordionItem key={module.id} value={module.id}>
       <AccordionTrigger>
-        <div className="grid grid-cols-8 gap-2 font-light items-center">
+        <div className="grid grid-cols-8 gap-2 font-light items-center w-full">
           <div className="flex flex-row gap-2 col-span-7 items-center">
             <div className="p-3 bg-blue-100 rounded-full">
               <FolderOpen className="size-8 text-blue-700 stroke-1" />
@@ -16,7 +16,7 @@ function ModuleItemList({ module, index }) {
             <span className=" text text-green-600 ">Módulo {index + 1}:</span>
             <span className="text-gray-500 text">{module.title}</span>
           </div>
-          <div className="flex flex-row gap-2 col-span-1">
+          <div className="flex flex-row gap-2 col-span-1 justify-end">
             <Edit2Icon
               className="w-6 h-6 stroke-1 text-blue-700"
               onClick={(event) => {
@@ -32,7 +32,7 @@ function ModuleItemList({ module, index }) {
               }}
             />
             <EyeIcon
-              className="w-6 h-6 stroke-1 text-green-700"
+              className="w-6 h-6 stroke-1 text-green-700 mr-3"
               onClick={(event) => {
                 event.preventDefault();
                 alert("hey view");

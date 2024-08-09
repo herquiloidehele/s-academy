f49070686abc0475265da776121c44231d6f5723
Ercilio Marques <erciliomarquesmanhica@gmail.com>
2024-08-09 12:08:46 +0200

    reduce panding top

--- src/app/tutor/summary/page.tsx
+++ src/app/tutor/summary/page.tsx
@@ -51,7 +51,7 @@ function TeacherSummary() {
   }
 
   return (
-    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto flex flex-col gap-6">
+    <div className="max-w-[85rem] px-4 py-4 mx-auto flex flex-col gap-6">
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
         <SummaryCard
           icon={<UsersIcon className="shrink-0 w-5 h-5 text-blue-500" />}
